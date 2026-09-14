2825f70116984c50fc2c05322eb8e78fa55548ac
Khayla <khayla.syafira@ui.ac.id>
2026-09-14 21:38:18 +0700

    register skills url and implement show_skills view

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from main.models import Experience
+from main.models import Experience, Skill
 
 def show_main(request):
     context = {
@@ -7,7 +7,7 @@ def show_main(request):
         "npm": "2506656910",
         "study_program": "S1 Sistem Informasi",  
         "bio": (
-            "IS Student at Universitas Indonesia. Driven by curiosity and ambition to shape the future through technology."
+            "IS Student at Universitas Indonesia. Driven by curiosity and ambition to shape the future through technology. "
             "A relentless lifelong learner, always seeking to grow, create, and turn ideas into meaningful impact."
         ),
     }
@@ -19,3 +19,12 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_skills(request):
+    context = {
+        "name": "Khayla Syafira Ardiasih",
+        "hard_skills": Skill.objects.filter(category="hard"),
+        "soft_skills": Skill.objects.filter(category="soft"),
+        "skill_list": Skill.objects.all(),
+    }
+    return render(request, "skills.html", context)
